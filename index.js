13bc51e3b59b892311aeefb96844d70937caed09
fede-rodes <federodes@gmail.com>
2021-12-28 17:14:54 +0100

    update deploy script using ren pool factory

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const RenPool = require('./artifacts/contracts/RenPool.sol/RenPool.json');
+const RenPool= require('./artifacts/contracts/RenPool.sol/RenPool.json');
+const RenPoolFactory = require('./artifacts/contracts/RenPoolFactory.sol/RenPoolFactory.json');
 const IERC20Standard = require('./artifacts/@renproject/gateway-sol/contracts/Gateway/interfaces/IERC20Standard.sol/IERC20Standard.json');
 const IDarknodeRegistry = require('./artifacts/interfaces/IDarknodeRegistry.sol/IDarknodeRegistry.json');
 const deployments = require('./ren-deployments.js');
 
 module.exports = {
     RenPool,
+    RenPoolFactory,
     IERC20Standard,
     IDarknodeRegistry,
     deployments,
